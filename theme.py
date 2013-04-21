de337633374957eb596fcc22ee73c686fec7d078
bingdian <imbingdian@gmail.com>
2013-04-21 15:49:56 +0800

    add theme files

---
+++ theme.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+name = 'responsive'
+version = '0.1'
+website = 'http://wlog.cn/'
+author = {
+    'name': 'bingdian',
+    'website': 'http://wlog.cn/',
+}
+
+lang = 'en'
+    
+twitter = None
+github = None
+disqus = None
+analytics = None
+
+
+allow_comment_on_secret_post = False
+
+
+nav = [
+    {'title': 'Home', 'link': '/'},
+    {'title': 'About', 'link': '/about.html'},
+]
+
+#: key-value descriptions
+descriptions = {}
